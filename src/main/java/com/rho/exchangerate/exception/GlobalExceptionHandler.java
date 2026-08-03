3041b73eca5475ee6a3c577f3f2654c714afc8fb
davidvieira51756-code <davidvieira51756@gmail.com>
2026-08-03 16:37:41 +0100

    Add global exception handling

---
+++ src/main/java/com/rho/exchangerate/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.rho.exchangerate.exception;
+
+import com.rho.exchangerate.dto.ApiErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(
+            IllegalArgumentException exception) {
+
+        ApiErrorResponse errorResponse = new ApiErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+}
