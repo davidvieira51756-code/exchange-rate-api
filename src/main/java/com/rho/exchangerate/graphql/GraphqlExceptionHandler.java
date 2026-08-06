f4aad7a109d4323f9079f01714dda780a81c32f0
davidvieira51756-code <davidvieira51756@gmail.com>
2026-08-06 16:14:41 +0100

    Add GraphQL exception   handling

---
+++ src/main/java/com/rho/exchangerate/graphql/GraphqlExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.rho.exchangerate.graphql;
+
+import com.rho.exchangerate.exception.ExchangeRateProviderException;
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.graphql.execution.ErrorType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GraphqlExceptionHandler
+        extends DataFetcherExceptionResolverAdapter {
+
+    @Override
+    protected GraphQLError resolveToSingleError(
+            Throwable exception,
+            DataFetchingEnvironment environment
+    ) {
+        if (exception instanceof IllegalArgumentException) {
+            return GraphqlErrorBuilder.newError(environment)
+                    .message(exception.getMessage())
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .build();
+        }
+
+        if (exception instanceof ExchangeRateProviderException) {
+            return GraphqlErrorBuilder.newError(environment)
+                    .message(exception.getMessage())
+                    .errorType(ErrorType.INTERNAL_ERROR)
+                    .build();
+        }
+
+        return null;
+    }
+}
